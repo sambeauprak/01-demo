bdd43d285392d558d859a33f16abe773b36d65e7
Sambeau <sambeauprak@gmail.com>
2025-11-17 16:34:46 +0100

    Format JSON data in demo.json and update server response to serve JSON content

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
+import { readFile } from "fs";
 import { createServer } from "http";
+// import json from "./demo.json" with { type: "json" };
 
 const server = createServer(function (request, response) {
   // Routing
   switch (request.url) {
     case "/":
-      response.writeHead(200, {
-        "Content-Type": "text/plain; charset=utf-8",
+      readFile("demo.json", "utf8", function (err, data) {
+        response.writeHead(200, {
+          "Content-Type": "application/json; charset=utf-8",
+        });
+        response.end(data);
       });
-      response.end("Page d'accueil");
+
       break;
 
     default:
@@ -21,5 +26,5 @@ const server = createServer(function (request, response) {
 
 const port = 3000;
 server.listen(port, function () {
-  console.log("Votre serveur est lancé");
+  console.log("Votre serveur est lancé sur http://localhost:" + 3000);
 });
